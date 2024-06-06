1140a74a3a42618888a3d3c4a610075d1d552a88
Akshitha2106 <akshithamittapalli@gmal.com>
2024-06-06 23:29:05 +0530

    Styled order page

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,12 +5,12 @@ function Button({ children, disabled, to, type }) {
   // const className =
   //   "bg-yellow-500 uppercase font-semibold text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
   const base =
-    "bg-yellow-500 uppercase font-semibold text-stone-800  inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+    "text-sm bg-yellow-500 uppercase font-semibold text-stone-800  inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
   const styles = {
     primary: base + "py-3 px-4 md:px-6 md:py-4",
     small: base + "py-2 px-4 md:px-4 md:py-2.5 text-xs",
     secondary:
-      "border-2 border-stone-300 uppercase font-semibold text-stone-400 py-2.5 px-4 inline-block rounded-full hover:text-stone-800 hover:bg-stone-300 focus:bg-stone-300 transition-colors duration-300 focus:outline-none  focus:text-stone-800 focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-3.5",
+      "text-sm border-2 border-stone-300 uppercase font-semibold text-stone-400 py-2.5 px-4 inline-block rounded-full hover:text-stone-800 hover:bg-stone-300 focus:bg-stone-300 transition-colors duration-300 focus:outline-none  focus:text-stone-800 focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-3.5",
   };
   if (to)
     return (
