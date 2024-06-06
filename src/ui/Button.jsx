f1a61626e4997a9d3d71d03eed9c5d84979779e8
Akshitha2106 <akshithamittapalli@gmal.com>
2024-06-06 16:52:06 +0530

    Styled Cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -9,6 +9,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + "py-3 px-4 md:px-6 md:py-4",
     small: base + "py-2 px-4 md:px-4 md:py-2.5 text-xs",
+    secondary:
+      "border-2 border-stone-300 uppercase font-semibold text-stone-400 py-2.5 px-4 inline-block rounded-full hover:text-stone-800 hover:bg-stone-300 focus:bg-stone-300 transition-colors duration-300 focus:outline-none  focus:text-stone-800 focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-3.5",
   };
   if (to)
     return (
